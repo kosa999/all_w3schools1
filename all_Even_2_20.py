efb2e204288771b7a7fe61c3156e60ca273fda10
kosa <aitmihoubbayram@gmail.com>
2025-06-04 12:42:41 +0500

    Описание изменений

--- all_Even_2_20.py
+++
@@ -1,5 +0,0 @@
-for i in range(2,21,2):
-    print (i)
-
-for i in range(2,21,2):
-    print (i, end=' ')
---
+++ task1_1_all_Even_2_20.py
@@ -0,0 +1,10 @@
+for i in range(2,21,2):
+    print (i)
+
+for i in range(2,21,2):
+    print (i, end =' ')
+
+print ( 'drugoi method ')
+for i in range (2,21):
+    if i % 2 == 0:
+        print (i, end = ' ')
